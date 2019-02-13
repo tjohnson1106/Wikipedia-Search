56fa99885026205ad7777d7e52ddd6175b6b9019
Thomas Johnson <tjohnson1106@gmail.com>
2019-02-13 16:32:20 -0500

    created initial fetch function

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const dotenv = require("dotenv");
-dotenv.config();
+const API_KEY =
+  "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+
+const API_URL = "https://en.wikipedia.org/?curid=";
+
+((global) => {
+  fetch(API_KEY)
+    .json()
+    .then((res) => console.log(res));
+  console.log(res);
+})(window);
--- src/data/api.js
+++
@@ -1,4 +0,0 @@
-const API_KEY =
-  "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
-
-const API_URL = "https://en.wikipedia.org/?curid=";
