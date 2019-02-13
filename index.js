0efee7954085eb03b73057eb148b9e4956e8336f
Thomas Johnson <tjohnson1106@gmail.com>
2019-02-13 17:17:14 -0500

    created display search method

--- index.js
+++ index.js
@@ -3,9 +3,43 @@ const API_KEY =
 
 const API_URL = "https://en.wikipedia.org/?curid=";
 
+const search = (term) => {
+  fetchJsonp(`${API_KEY}&gsrsearch=${term}`)
+    .then((res) => res.json())
+    .then((data) => displaySearch(data.query.pages))
+    .catch((err) => console.log(err));
+};
+
+const displaySearch = (obj) => {
+  document.getElementById("results").innerHTML = "";
+  for (i in obj) {
+    displayResults(obj[i]);
+  }
+};
+
+const displayResults = (obj) => {
+  document.getElementById("results").innerHTML += `
+      <li>
+        <div class="card">
+          <h3>
+            ${obj.title}
+      
+          </h3>
+        <p>
+          ${obj.extract}
+        </p>
+          <a href="${API_URL}${obj.pageid}">
+            Read More
+          </a>
+        </div>
+      </li>
+    `;
+};
+
 ((global) => {
-  fetch(API_KEY)
-    .json()
-    .then((res) => console.log(res));
-  console.log(res);
+  document.getElementById("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("input value?", e.target[0].value);
+    search(e.target[0].value);
+  });
 })(window);
