ed2b380273ed4c9126fac09baa0594fb98a91a28
Thomas Johnson <tjohnson1106@gmail.com>
2019-02-13 16:21:33 -0500

    created wikipedia api configuration

---
+++ src/data/api.js
@@ -0,0 +1,4 @@
+const API_KEY =
+  "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+
+const API_URL = "https://en.wikipedia.org/?curid=";
